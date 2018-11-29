9252bb2266c913bda42b1639a066a1f06371bb21
gs <mlovesm@naver.com>
2018-11-29 17:51:18 +0900

    first commit

---
+++ src/main/java/com/example/boot/SecurityBoardApplication.java
@@ -0,0 +1,12 @@
+package com.example.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityBoardApplication.class, args);
+	}
+}
