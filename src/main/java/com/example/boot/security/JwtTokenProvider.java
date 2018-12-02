75ade170439fc59f8dbeac4aa068687cb315b4ae
mlovesm <mlovesm@naver.com>
2018-12-03 00:32:46 +0900

    JWT 수정

--- src/main/java/com/example/boot/security/JwtTokenProvider.java
+++ src/main/java/com/example/boot/security/JwtTokenProvider.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
-import java.nio.file.attribute.UserPrincipal;
 import java.util.Date;
 
 
