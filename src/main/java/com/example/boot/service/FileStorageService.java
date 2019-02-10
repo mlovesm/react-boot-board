1c2017fc5b7a94584da0ad44fc919789acaf55a2
mlovesm <mlovesm@naver.com>
2019-02-11 01:17:35 +0900

    파일 저장 설정 중

--- src/main/java/com/example/boot/service/FileStorageService.java
+++ src/main/java/com/example/boot/service/FileStorageService.java
@@ -1,11 +1,14 @@
 package com.example.boot.service;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.text.DecimalFormat;
+import java.util.Calendar;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
@@ -18,14 +21,17 @@ import com.example.boot.exception.FileStorageException;
 import com.example.boot.exception.MyFileNotFoundException;
 import com.example.boot.file.FileStorageProperties;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class FileStorageService {
 
     private final Path fileStorageLocation;
-
+    
     @Autowired
     public FileStorageService(FileStorageProperties fileStorageProperties) {
-        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir())
+        this.fileStorageLocation = Paths.get(fileStorageProperties.getUploadDir() + calcPath(""))
                 .toAbsolutePath().normalize();
 
         try {
@@ -35,18 +41,60 @@ public class FileStorageService {
         }
     }
 
-    public String storeFile(MultipartFile file) {
-        // Normalize file name
+    
+	//폴더 생성 함수
+	@SuppressWarnings("unused")
+	private static String calcPath(String uploadPath) {
+		Calendar cal = Calendar.getInstance();
+		
+		String order = "VOD";
+		String yearPath = order + File.separator + cal.get(Calendar.YEAR);
+		String monthPath = yearPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.MONTH)+1);
+		String datePath = monthPath + File.separator + new DecimalFormat("00").format(cal.get(Calendar.DATE));
+		
+		makeDir(uploadPath, order, yearPath, monthPath, datePath);
+		
+		log.info(datePath);
+		
+		return datePath;
+	}//calcPath
+	
+	//폴더 생성 함수
+	private static void makeDir(String uploadPath, String... paths) {
+		System.out.println(uploadPath + paths[paths.length -1]);
+		if(new File(uploadPath + paths[paths.length -1]).exists()) {
+			return;
+		}//if
+		
+		for(String path : paths) {
+			
+			Path dirPath = Paths.get(uploadPath + path);
+			
+			try {
+				Files.createDirectories(dirPath);
+			} catch (IOException e) {
+				e.printStackTrace();
+				throw new FileStorageException("Could not create the directory where the uploaded files will be stored.", e);
+			}
+			
+		}//for
+		
+	}//makeDir
+
+    public String storeFile(MultipartFile file) { 
+    	//calcPath("");
+    	System.out.println("fileStorageLocation="+fileStorageLocation);
         String fileName = StringUtils.cleanPath(file.getOriginalFilename());
 
         try {
-            // Check if the file's name contains invalid characters
+            // 파일 이름에 잘못된 문자가 포함되어 있는지
             if(fileName.contains("..")) {
                 throw new FileStorageException("Sorry! Filename contains invalid path sequence " + fileName);
             }
 
-            // Copy file to the target location (Replacing existing file with the same name)
+            // 대상 위치로 파일 복사(동일한 이름으로 기존 파일 교체)
             Path targetLocation = this.fileStorageLocation.resolve(fileName);
+            System.out.println(targetLocation);
             Files.copy(file.getInputStream(), targetLocation, StandardCopyOption.REPLACE_EXISTING);
 
             return fileName;
